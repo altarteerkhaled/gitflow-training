840495125022fcfd272923c583517ffed2abd301
altarteerkhaled <altarteer.khaled@gmail.com>
2025-01-12 22:22:17 +0300

    developing feature2

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String[] args) {
         // This is the dev branch
     }
+
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
 }
